4a267bfd19b571626a7b5b3251138350b5f3189b
dasha379 <hjfktd@yandex.ru>
2025-10-31 20:41:39 +0300

    added the  second part

--- third_task.cpp
+++ third_task.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <random>
 #include <algorithm>
+#include <set>
 
 // генерация произвольного неориентированного графа с заданными характеристиками:
 // n - число вершин
@@ -11,10 +12,10 @@ class Graph{
 private:
     int n;
     double p;
-    std::vector<std::vector<int>> adj;
-    std::vector<std::pair<int, int>> edgeList;
     std::mt19937 gen;
 public:
+    std::vector<std::vector<int>> adj;
+    std::vector<std::pair<int, int>> edgeList;
 
     Graph(int vertices, double density) : n(vertices), p(density), adj(n, std::vector<int>(n, 0)){
         generateGraph();
@@ -88,13 +89,77 @@ public:
 
 };
 
+class TurGraph : public Graph{
+private:
+    int n;
+    int r;
+    std::vector<std::vector<int>> partitions;
+public:
+    TurGraph(int vert, int part) : n(vert), r(part), Graph(n, 0.3) {
+        generatePartitions();
+        generateTuranGraph();
+    }
+
+    void generatePartitions(){
+        partitions.clear();
+        partitions.resize(r);
+
+        for (int i = 0; i < n; ++i){
+            int ind = i % r;
+            partitions[ind].push_back(i);
+        }
+    }
+
+    void generateTuranGraph(){
+        edgeList.clear();
+
+        double e = (r - 1)*n*n/(2*r);
+        int edges = std::floor(static_cast<int>(e));
+        int size_up = std::ceil(n/r);
+        int size_down = std::floor(n/r);
+        // количество долей с размером size_up and size_down соответственно
+        int k1 = n % r;
+        int k2 = r - k1;
+
+        generatePartitions();
+        for (int i = 0; i < r; ++i){
+            for (int j = i + 1; j < r; ++i){
+                for (const auto& u : partitions[i]){
+                    for (const auto& v : partitions[j]){
+                        adj[u][v] = 1;
+                        adj[v][u] = 1;
+                        edgeList.push_back({u, v});
+                    }
+                }
+            }
+        }
+    }
+
+    void generateMoonMoser(int m){
+        int n = 3*m;
+        int r = m;
+        generateTuranGraph();
+    }
+
+    ~TurGraph(){};
+
+};
+
 int main(){
     std::cout << "=== Генерация случайного графа ===\n";
     int vertices = 8;
     double density = 0.6;
-    
+
     Graph g1(vertices, density);
     g1.stats();
     g1.printGraph();
     g1.printEdges();
+
+    std::cout << "=== генерация графа Турана ===\n";
+    int v = 7;
+    int r = 3;
+    TurGraph tur_g(v, r);
+    tur_g.Graph::stats();
+    tur_g.Graph::printEdges();
+
 }
